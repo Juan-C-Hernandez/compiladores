b1aea10a7314a724f0eead3f0d3de9ac71f0ce82
Juan C. Hernandez <42014007+Juan-C-Hernandez@users.noreply.github.com>
2020-06-18 21:47:30 -0500

    Update backpatch.h

--- codigoIntermedio/backpatch.h
+++ codigoIntermedio/backpatch.h
@@ -1,3 +1,6 @@
+#ifndef BACKPACK_H
+#define BACKPACK_H
+
 #include "cuadruplas.h"
 
 typedef struct index INDEX;
@@ -7,19 +10,21 @@ struct index{
     INDEX *next;
 };
 
-typedef struct list index {
+typedef struct list_index {
     INDEX *head;
     INDEX *tail;
 
 }LINDEX;
 
-INDEX *init_index();
-void finish_index(INDEX *i);
+INDEX *init_index(); // Reserva memoria para un nodo de indice
+void finish_index(INDEX *i); // Libera la memoria de un nodo indice
 
-LINDEX *init_list_index(INDEX *i);
-void finish_list_index(CODE *c);
+LINDEX *init_list_index(INDEX *i); // Reserva memoria para la lista de indice se inserta el primero
+void finish_list_index(LINDEX *L); // Libera la memoria de la lista y de todos los nodos dentro
 
-void append_index(LINDEX *l, INDEX *i);
+void append_index(LINDEX *l, INDEX *i); // Agrega un nodo indice al final de la lista
 
-LINDEX *combinar(LINDEX l1, LINDEX l2);
-void backpath(CODE *c LINDEX l, char *label);
+LINDEX *combinar(LINDEX l1, LINDEX l2); // retorna una lista ligada de la concatenacion de l1 con l2
+void backpath(CODE *c, LINDEX l, char *label); // Reemplaza label en cada aparicion de un indice en la  cuadruplas del codigo c
+
+#endif
