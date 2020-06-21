5cc430017b96a74a99b56969c44a8eccf3792f45
Juan C. Hernandez <42014007+Juan-C-Hernandez@users.noreply.github.com>
2020-06-21 17:28:15 -0500

    Update tabla_simbolos.h

--- estructuras/tabla_simbolos.h
+++ estructuras/tabla_simbolos.h
@@ -6,8 +6,8 @@ void append_arg(ARGS *args, int arg); //Agrega un argumento al final
 int compare_args(ARGS *a1, ARGS *a2); //Compara dos lista y devuelve 1 si son iguales, 0 si son diferentes
 void append_sym(SYMTAB *t, SYM *s); //Agrega al final de la tabla un nuevo simbolo
 void clear_sym_tab(SYMTAB *t); // Deja vacia la tabla -------
-SYMTAB pop_st(SSTACK *s); // Ejecuta un pop sobre la pila de tablas de simbolos
-void push_st(SSTACK *s, SYMTAB *st); // Ingresa una tabla a la pila de tablas de simbolos
+SYMTAB pop_st_sym(SSTACK *s); // Ejecuta un pop sobre la pila de tablas de simbolos
+void push_st_sym(SSTACK *s, SYMTAB *st); // Ingresa una tabla a la pila de tablas de simbolos
 ARGS *init_args(); // Retorna e l apunador a un t ipo ARGS
 ARG *init_arg(int dato); // Reserva memoria para un t ipo ARG y retorna el apuntador
 SSTACK *init_sym_tab_stack(); // Reserva memoria para la pila
@@ -18,7 +18,7 @@ void finish_sym_tab(SYMTAB *st); // Libe ra memoria para una tabla de simbolos
 void finish_sym(SYM *s); // libera memoria para un simbolo
 void finish_arg(ARG *a); // libera memoria para un arg
 void finish_args(ARGS *a); // libera memoria para una lista ARGS
-void print_tab(SYMTAB *t); // Imprime en pantalla la tabla de simbolos
+void print_tab_sym(SYMTAB *t); // Imprime en pantalla la tabla de simbolos
 void print_args(ARGS *args);
 int getDir(SYMTAB *T, char *id);
 int getTipo(SYMTAB *T, char *id);
