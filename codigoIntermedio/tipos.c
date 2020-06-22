430c381e64a8fe02b7b0b08cfda4fedd85250881
Juan C. Hernandez <42014007+Juan-C-Hernandez@users.noreply.github.com>
2020-06-21 19:32:20 -0500

    Update tipos.c

--- codigoIntermedio/tipos.c
+++ codigoIntermedio/tipos.c
@@ -1,12 +1,12 @@
 #include "tipos.h"
-#include "..\estructuras\tabla_tipos.h"
+//#include "..\estructuras\tabla_tipos.h"
 
 /*
 * Recibe dos tipos , cada uno es indice hacia la tabla de tipos en la cima de la pila
 * retorna el indice de mayor dimension
 */
 int max(int t1, int t2) {
-	return getTam(pila_tabla_tipo->top, t1) > getTam(pila_tabla_tipo->top, t1) ? t1 : t2;
+	return getTam(pila_tabla_tipo->top, t1) > getTam(pila_tabla_tipo->top, t2) ? t1 : t2;
 }
 
 /*
@@ -14,5 +14,5 @@ int max(int t1, int t2) {
 * retorna el indice de menor dimension
 */
 int min(int t1, int t2) {
-	return getTam(pila_tabla_tipo->top, t1) < getTam(pila_tabla_tipo->top, t1) ? t1 : t2;
+	return getTam(pila_tabla_tipo->top, t1) < getTam(pila_tabla_tipo->top, t2) ? t1 : t2;
 }
