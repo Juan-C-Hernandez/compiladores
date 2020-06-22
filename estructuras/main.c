ba98cc632aadc1c9854148f244afc210ef9b2774
Juan C. Hernandez <42014007+Juan-C-Hernandez@users.noreply.github.com>
2020-06-21 19:34:14 -0500

    Update main.c

--- estructuras/main.c
+++ estructuras/main.c
@@ -2,202 +2,244 @@
 // Created by juan2 on 31/05/2020.
 //
 
-#include "tabla_tipos.h"
+#include "tabla_simbolos.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 
-void print_typ(TYP *tipo){
-    TYP *tmp = tipo;
-    while(tmp){
-        printf("ID: %d\nNombre: %s\nTam: %d\nMemoria %p\n\n", tmp->id, tmp->nombre, tmp->tam, tmp);
-        tmp = tmp->next;
-    }
-}
-
-void print_stack(TSTACK *s){
-    TYPTAB *tmp = s->top;
+void print_stack(SSTACK *s){
+    SYMTAB *tmp = s->top;
+    printf("------------------------------------------------\n");
     if(!tmp){
         printf("Nada que imprimir\n");
     } else{
-        printf("Imprimiendo pila:\n");
+        printf("Imprimiendo pila:\nTOP\n");
         while(tmp != NULL){
-        print_tab(tmp);
-        tmp = tmp->next;
-    }
+        	print_tab(tmp);
+        	printf("\n");
+        	tmp = tmp->next;
+    	}
     }
 }
 
 int main(int arc, char **argv){
-    
-    /* PRUEBA INIT */
-	TSTACK *pila = init_type_tab_stack();
-	if(!pila){
-	    printf("Error al crear la pila\n");
-	    return -1;
-    }
-    
-    TYPTAB *tabla = init_type_tab();
-    if(!pila){
-	    printf("Error al crear la tabla\n");
-	    return -1;
-    }
-    
-    /* PRUEBA DE TIPOS */
-    TYP *tipo1 = init_type();
-    TYP *tipo2 = init_type();
-    TYP *tipo3 = init_type();
-    
-    tipo1->id = 0;
-    strcpy(tipo1->nombre, "ent");
-    tipo1->tam = 8;
-    print_typ(tipo1);
-    
-    tipo2->id = 1;
-    strcpy(tipo2->nombre, "real");
-    tipo2->tam = 8;
-    print_typ(tipo2);
-    
-    tipo3->id = 2;
-    strcpy(tipo3->nombre, "dreal");
-    tipo3->tam = 16;
-    print_typ(tipo3);
-    printf("\n");
-    
-    /* PRUEBA APPEND TABLAS */
-    printf("Imprimiendo tabla vacia\n");
-    print_tab(tabla);
-    printf("\n");
-    
-    append_type(tabla, tipo1);
-    printf("Se salio de append, la lista tiene ahora %d elementos\n\n", getNum(tabla));
-    
-    append_type(tabla, tipo2);
-    printf("Se salio de append, la lista tiene ahora %d elementos\n\n", getNum(tabla));
-    
-    append_type(tabla, tipo3);
-    printf("Se salio de append, la lista tiene ahora %d elementos\n\n", getNum(tabla));
-    
-    /* PRUEBA IMPRIMIR TABA */
-    print_tab(tabla);
-    
-    /* PRUEBA BUSQUEDAS */
-    int id_a_buscar = 2;
-    TYP *tipo_busqueda = busca_typ(tabla->head, id_a_buscar);
-    printf("Resultado de busqueda de id %d:\n", id_a_buscar);
-    if(!tipo_busqueda){
-        printf("El id %d no esta en la tabla\n", id_a_buscar);
-    } else{
-        print_typ(tipo_busqueda);
-    }
-    char *nombre_id_a_buscar = getNombre(tabla, id_a_buscar);
-    if(!nombre_id_a_buscar){
-        printf("El nombre de %d no se ha encontrado\n", id_a_buscar);
-    } else{
-        printf("El nombre de %d es %s\n", id_a_buscar, nombre_id_a_buscar);
-    }
-    int tam = getTam(tabla, id_a_buscar);
-    if(!tam){
-        printf("El tamaño de %d no se ha encontrado\n", id_a_buscar);
-    } else{
-        printf("El tamaño de %d es %d\n", id_a_buscar, tam);
-    }
-    printf("\n");
-    
-    id_a_buscar = 5;
-    tipo_busqueda = busca_typ(tabla->head, id_a_buscar);
-    printf("Resultado de busqueda de id %d:\n", id_a_buscar);
-    if(!tipo_busqueda){
-        printf("El id %d no esta en la tabla\n", id_a_buscar);
-    } else{
-        print_typ(tipo_busqueda);
-    }
-    char *nombre_id_a_buscar2 = getNombre(tabla, id_a_buscar);
-    if(!nombre_id_a_buscar2){
-        printf("El nombre de %d no se ha encontrado\n", id_a_buscar);
-    } else{
-        printf("El nombre de %d es %s\n", id_a_buscar, nombre_id_a_buscar2);
-    }
-    tam = getTam(tabla, id_a_buscar);
-    if(!tam){
-        printf("El tamaño de %d no se ha encontrado\n", id_a_buscar);
-    } else{
-        printf("El tamaño de %d es %d\n", id_a_buscar, tam);
-    }
-    printf("\n");
-    printf("\n");
-    
-    /* PRUEBA PILA */
-    printf("Imprimiendo pila vacia\n");
-    print_stack(pila);
-    printf("\n");
-    
-    TYPTAB *tabla2 = init_type_tab(); // INIT FUNCIONA
-    TYP *tipo4 = init_type();
-    TYP *tipo5 = init_type();
-    TYP *tipo6 = init_type();
-    
-    tipo4->id = 0;
-    strcpy(tipo4->nombre, "int");
-    tipo4->tam = 9;
-    
-    tipo5->id = 1;
-    strcpy(tipo5->nombre, "float");
-    tipo5->tam = 15;
-    
-    tipo6->id = 2;
-    strcpy(tipo6->nombre, "long");
-    tipo6->tam = 32;
-    
-    append_type(tabla2, tipo4);
-    append_type(tabla2, tipo5);
-    append_type(tabla2, tipo6);
-    
-    
-    TYPTAB *tabla3 = init_type_tab();
-    TYP *tipo7 = init_type();
-    TYP *tipo8 = init_type();
-    TYP *tipo9 = init_type();
-    TYP *tipo10 = init_type();
-    
-    tipo7->id = 0;
-    strcpy(tipo7->nombre, "cadena");
-    tipo7->tam = 64;
-    
-    tipo8->id = 1;
-    strcpy(tipo8->nombre, "arreglo");
-    tipo8->tam = 4;
-    
-    tipo9->id = 2;
-    strcpy(tipo9->nombre, "apuntador");
-    tipo9->tam = 2;
-    
-    tipo10->id = 3;
-    strcpy(tipo10->nombre, "size");
-    tipo10->tam = 3;
-    
-    append_type(tabla3, tipo7);
-    append_type(tabla3, tipo8);
-    append_type(tabla3, tipo9);
-    append_type(tabla3, tipo10);
-    
-    push_st(pila, tabla);
-    push_st(pila, tabla2);
-    push_st(pila, tabla3);
-    printf("Imprimiendo pila\n");
-    print_stack(pila);
-    printf("\n");
-    
-    clear_type_tab(tabla2);
-    printf("Imprimiendo pila despues de limpiar tabla %p\n", tabla2);
-    print_stack(pila);
-    printf("\n");
-    
-    printf("Pop a la pila\n");
-    TYPTAB popped = pop_st(pila);
-    printf("Imprimiendo pila despues de pop\n");
-    print_stack(pila);
-    printf("\n");
-    print_tab(&popped);
+    ARGS *args = init_args();
+	ARGS *args1 = init_args();
+	ARGS *args2 = init_args();
+	ARGS *args3 = init_args();
+	ARGS *args4 = init_args();
+	ARGS *args5 = init_args();
+	int i;
+	
+	append_arg(args, 0);
+	append_arg(args, 2);
+	append_arg(args, 3);
+	append_arg(args, 8);
+	append_arg(args, 90);
+	append_arg(args, 50);
+	
+	append_arg(args1, 0);
+	append_arg(args1, 2);
+	append_arg(args1, 33);
+	append_arg(args1, 8);
+	append_arg(args1, 90);
+	append_arg(args1, 50);
+	
+	print_args(args);
+	print_args(args1);
+	i = compare_args(args, args1);
+	if(i == 1){
+		printf("Son iguales\n");
+	} else {
+		printf("No son iguales\n");
+	}
+	
+	printf("\n");
+	
+	append_arg(args2, 0);
+	append_arg(args2, 2);
+	append_arg(args2, 3);
+	append_arg(args2, 8);
+	append_arg(args2, 90);
+	append_arg(args2, 50);
+	append_arg(args2, 50);
+	
+	print_args(args);
+	print_args(args2);
+	i = compare_args(args, args2);
+	if(i == 1){
+		printf("Son iguales\n");
+	} else {
+		printf("No son iguales\n");
+	}
+	printf("\n");
+	
+	print_args(args2);
+	print_args(args);
+	i = compare_args(args2, args);
+	if(i == 1){
+		printf("Son iguales\n");
+	} else {
+		printf("No son iguales\n");
+	}
+	printf("\n");
+	
+	append_arg(args4, 0);
+	append_arg(args4, 2);
+	append_arg(args4, 33);
+	append_arg(args4, 8);
+	append_arg(args4, 90);
+	append_arg(args4, 51);
+	
+	print_args(args);
+	print_args(args4);
+	i = compare_args(args, args4);
+	if(i == 1){
+		printf("Son iguales\n");
+	} else {
+		printf("No son iguales\n");
+	}
+	printf("\n");
+	
+	append_arg(args5, 1);
+	append_arg(args5, 2);
+	append_arg(args5, 33);
+	append_arg(args5, 8);
+	append_arg(args5, 90);
+	append_arg(args5, 50);
+	
+	print_args(args);
+	print_args(args5);
+	i = compare_args(args, args5);
+	if(i == 1){
+		printf("Son iguales\n");
+	} else {
+		printf("No son iguales\n");
+	}
+	printf("\n");
+	
+	append_arg(args3, 0);
+	append_arg(args3, 2);
+	append_arg(args3, 3);
+	append_arg(args3, 8);
+	append_arg(args3, 90);
+	append_arg(args3, 50);
+	print_args(args);
+	print_args(args3);
+	i = compare_args(args, args3);
+	if(i == 1){
+		printf("Son iguales\n");
+	} else {
+		printf("No son iguales\n");
+	}
+	printf("\n");
+	
+	finish_args(args5);
+	
+	SYMTAB *tabla_simbolos = init_sym_tab();
+	if(!tabla_simbolos) {
+		printf("Ha ocurrido un error al reservar memoria para la tabla\n");
+	}
+	
+	SYM *simbolo1 = init_sym();
+	if(!simbolo1) {
+		printf("Ha ocurrido un error al reservar memoria para el simbolo\n");
+	}
+	
+	SYM *simbolo2 = init_sym();
+	if(!simbolo2) {
+		printf("Ha ocurrido un error al reservar memoria para el simbolo\n");
+	}
+	
+	SYM *simbolo3 = init_sym();
+	if(!simbolo3) {
+		printf("Ha ocurrido un error al reservar memoria para el simbolo\n");
+	}
+	
+	simbolo1->dir = 0;
+	simbolo1->tipo = 0;
+	strcpy(simbolo1->id, "x");
+	simbolo1->args = args;
+	simbolo1->num = 6;
+	simbolo1->var = 0;
+	
+	simbolo2->dir = 4;
+	simbolo2->tipo = 4;
+	strcpy(simbolo2->id, "y");
+	simbolo2->args = args1;
+	simbolo2->num = 6;
+	simbolo2->var = 1;
+	
+	simbolo3->dir = 8;
+	simbolo3->tipo = 2;
+	strcpy(simbolo3->id, "kjh");
+	simbolo3->args = args3;
+	simbolo3->num = 6;
+	simbolo3->var = 3;
+	
+	append_sym(tabla_simbolos, simbolo1);
+	append_sym(tabla_simbolos, simbolo2);
+	append_sym(tabla_simbolos, simbolo3);
+	
+	SSTACK *pila = init_sym_tab_stack();
+	if(!pila) {
+		printf("Ha ocurrido un error al reservar memoria para la pila\n");
+	}
+	
+	SYMTAB *tabla_simbolos2 = init_sym_tab();
+	clear_sym_tab(tabla_simbolos2);
+	SYMTAB *tabla_simbolos3 = init_sym_tab();
+	clear_sym_tab(tabla_simbolos3);
+	SYMTAB *tabla_simbolos4 = init_sym_tab();
+	clear_sym_tab(tabla_simbolos4);
+	
+	push_st(pila, tabla_simbolos);
+	push_st(pila, tabla_simbolos2);
+	push_st(pila, tabla_simbolos3);
+	push_st(pila, tabla_simbolos4);
+	printf("PILA\n");
+	print_stack(pila);
+	
+	SYMTAB p = pop_st(pila);
+	printf("\nPila despues de pop\n");
+	print_stack(pila);
+	
+	//char id[4] = "kjh";
+	//char id[2] = "x";
+	char id[2] = "y";
+	
+	printf("\n------------------------------------------\n");
+	int b = getDir(tabla_simbolos, id);
+	if(b >= 0){
+		printf("La direccion es: %d\n", b);
+	} else {
+		printf("No se ha encontrado el id\n");
+	
+	}
+	b = getTipo(tabla_simbolos, id);
+	if(b >= 0){
+		printf("El tipo es es: %d\n", b);
+	} else {
+		printf("No se ha encontrado el id\n");
+	
+	}
+	b = getVar(tabla_simbolos, id);
+	if(b >= 0){
+		printf("La variable es: %d\n", b);
+	} else {
+		printf("No se ha encontrado el id\n");
+	
+	}
+	b = getNumArgs(tabla_simbolos, id);
+	if(b >= 0){
+		printf("El numero de argumentos es: %d\n", b);
+	} else {
+		printf("No se ha encontrado el id\n");
+	
+	}
+	printf("------------------------------------------\n");
+	
+	clear_sym_tab(tabla_simbolos);
+	printf("Pila despues de clear_sym_tab\n");
+	print_stack(pila);
 }
